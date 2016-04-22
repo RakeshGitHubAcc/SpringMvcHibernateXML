0e2856ef5a864fa0dc4e87f04db05832477ecc98
RakeshGitHubAcc <bishwal.rakeshkumar87@gmail.com>
2016-04-22 18:09:07 +0530

    Spring Mvc Hibernate XML
    First Commit

---
+++ src/com/spring/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.spring.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.dao.UserDAO;
+import com.spring.model.User;
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class UserController {
+	@Autowired
+	private UserDAO userDao;
+	 @RequestMapping(value="/")
+	 public ModelAndView home() {
+	        List<User> listUsers = userDao.list();
+	        ModelAndView model = new ModelAndView("home");
+	        model.addObject("userList", listUsers);
+	        return model;
+	    }
+}
